69eff691eb7d9befcbb3d64f1ca3cc52b33cc182
Paulo-Dandrea <prog.dandrea@gmail.com>
2022-02-12 18:03:11 -0300

    feat: add mysql connector and basic services

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+
+# Mind use of anaconda. Now, pip is using anaconda and vscode as well.
+# Before, vscode was not using anaconda.
+import mysql.connector
+import json
 from flask import Flask
+
 app = Flask(__name__)
 
 
@@ -6,5 +12,60 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, Docker!'
 
-# Mind use of anaconda. Now, pip is using anaconda and vscode as well. 
-# Before, vscode was not using anaconda.
+
+@app.route('/widgets')
+def get_widgets():
+    mydb = mysql.connector.connect(
+        host="mysqldb",
+        user="root",
+        password="p@ssw0rd1",
+        database="inventory"
+    )
+    cursor = mydb.cursor()
+
+    cursor.execute("SELECT * FROM widgets")
+
+    # this will extract row headers
+    row_headers = [x[0] for x in cursor.description]
+
+    results = cursor.fetchall()
+    json_data = []
+    for result in results:
+        json_data.append(dict(zip(row_headers, result)))
+
+    cursor.close()
+
+    return json.dumps(json_data)
+
+
+@app.route('/initdb')
+def db_init():
+    mydb = mysql.connector.connect(
+        host="mysqldb",
+        user="root",
+        password="p@ssw0rd1"
+    )
+    cursor = mydb.cursor()
+
+    cursor.execute("DROP DATABASE IF EXISTS inventory")
+    cursor.execute("CREATE DATABASE inventory")
+    cursor.close()
+
+    mydb = mysql.connector.connect(
+        host="mysqldb",
+        user="root",
+        password="p@ssw0rd1",
+        database="inventory"
+    )
+    cursor = mydb.cursor()
+
+    cursor.execute("DROP TABLE IF EXISTS widgets")
+    cursor.execute(
+        "CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
+    cursor.close()
+
+    return 'init database'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
