1efb8ec57e1c17c8fe56a1b664eba4a86f2f3f0e
Paulo-Dandrea <prog.dandrea@gmail.com>
2022-02-14 20:35:51 -0300

    feat: db column names

--- db.py
+++ db.py
@@ -4,30 +4,41 @@ from mysql.connector import errorcode
 
 
 def get_candidates():
-    mydb = mysql.connector.connect(
-        host="mysqldb",
-        user="root",
-        password="p@ssw0rd1",
-        database="sample_university"
-    )
+    try:
+        mydb = mysql.connector.connect(
+            host="mysqldb",
+            user="root",
+            password="p@ssw0rd1",
+            database="sample_university"
+        )
 
-    cursor = mydb.cursor()
+        cursor = mydb.cursor()
 
-    cursor.execute("SELECT * FROM candidates")
+        cursor.execute("SELECT * FROM candidates")
 
-    # this will extract row headers
-    row_headers = [x[0] for x in cursor.description]
+        # this will extract row headers
+        row_headers = [x[0] for x in cursor.description]
 
-    results = cursor.fetchall()
-    json_data = []
-    for result in results:
-        json_data.append(dict(zip(row_headers, result)))
+        results = cursor.fetchall()
+        json_data = []
+        for result in results:
+            json_data.append(dict(zip(row_headers, result)))
 
-    cursor.close()
+        cursor.close()
 
-    return json.dumps(json_data)
+        return json.dumps({'candidates': json_data, 'size': len(json_data)})
 
-def add_candidate(widget, description):
+    # Provavelmente tem uma forma de reaproveitar este except para todos os usos
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print('err', err)
+
+
+def add_candidate(name, score):
     try:
         cnx = mysql.connector.connect(
             host="mysqldb",
@@ -38,10 +49,10 @@ def add_candidate(widget, description):
         cursor = cnx.cursor()
 
         add_candidates = ("INSERT INTO candidates "
-                          "(name, description) "
+                          "(name, score) "
                           "VALUES (%s, %s)")
 
-        data_candidates = (widget, description)
+        data_candidates = (name, score)
 
         cursor.execute(add_candidates, data_candidates)
 
@@ -60,33 +71,40 @@ def add_candidate(widget, description):
         else:
             print('err', err)
 
+
 def db_init():
-    mydb = mysql.connector.connect(
-        host="mysqldb",
-        user="root",
-        password="p@ssw0rd1"
-    )
-    cursor = mydb.cursor()
+    try:
+        mydb = mysql.connector.connect(
+            host="mysqldb",
+            user="root",
+            password="p@ssw0rd1"
+        )
+        cursor = mydb.cursor()
 
-    cursor.execute("DROP DATABASE IF EXISTS sample_university")
-    cursor.execute("CREATE DATABASE sample_university")
-    cursor.close()
+        cursor.execute("DROP DATABASE IF EXISTS sample_university")
+        cursor.execute("CREATE DATABASE sample_university")
+        cursor.close()
 
-    mydb = mysql.connector.connect(
-        host="mysqldb",
-        user="root",
-        password="p@ssw0rd1",
-        database="sample_university"
-    )
-    cursor = mydb.cursor()
+        mydb = mysql.connector.connect(
+            host="mysqldb",
+            user="root",
+            password="p@ssw0rd1",
+            database="sample_university"
+        )
+        cursor = mydb.cursor()
 
-    cursor.execute("DROP TABLE IF EXISTS candidates")
-    cursor.execute(
-        "CREATE TABLE candidates (name VARCHAR(255), description VARCHAR(255))")
-    cursor.close()
+        cursor.execute("DROP TABLE IF EXISTS candidates")
+        cursor.execute(
+            "CREATE TABLE candidates (name VARCHAR(255), score VARCHAR(255))")
+        cursor.close()
 
-    return 'init database'
+        return 'init database'
 
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print('err', err)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
