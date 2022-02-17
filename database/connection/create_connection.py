41f8fb433d6d4bd64a8a7157d4aee8be59d8cda4
Paulo-Dandrea <prog.dandrea@gmail.com>
2022-02-16 21:09:06 -0300

    chore: .env

--- database/connection/create_connection.py
+++ database/connection/create_connection.py
@@ -1,15 +1,30 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 import mysql.connector
 from mysql.connector import errorcode
+
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+HOST = os.getenv('HOST')
+PASSWORD = os.environ.get("PASSWORD")
+USER = os.environ.get("USER")
+
 
 def create_connection():
     try:
         cnx = mysql.connector.connect(
-            host="mysqldb",
-            user="root",
-            password="p@ssw0rd1",
+            host=HOST,
+            user=USER,
+            password=PASSWORD,
             database="sample_university"
         )
+
         return cnx
+        
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
--- database/connection/init_db.py
+++ database/connection/init_db.py
@@ -1,14 +1,27 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 import mysql.connector
 from mysql.connector import errorcode
+
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+HOST = os.getenv('HOST')
+PASSWORD = os.environ.get("PASSWORD")
+USER = os.environ.get("USER")
 
 
 def db_init():
     try:
         mydb = mysql.connector.connect(
-            host="mysqldb",
-            user="root",
-            password="p@ssw0rd1"
+            host=HOST,
+            user=USER,
+            password=PASSWORD,
         )
+
         cursor = mydb.cursor()
 
         cursor.execute("DROP DATABASE IF EXISTS sample_university")
@@ -16,11 +29,12 @@ def db_init():
         cursor.close()
 
         mydb = mysql.connector.connect(
-            host="mysqldb",
-            user="root",
-            password="p@ssw0rd1",
+            host=HOST,
+            user=USER,
+            password=PASSWORD,
             database="sample_university"
         )
+        
         cursor = mydb.cursor()
 
         cursor.execute("DROP TABLE IF EXISTS candidates")
